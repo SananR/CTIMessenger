3373414eeb58f1059d04cccb939eec376025af09
SananR <raosanan@gmail.com>
2025-03-16 20:23:40 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   config.enable_reloading = true
 
   config.serve_static_assets = true
+  config.assets.compile = true
 
   # Do not eager load code on boot.
   config.eager_load = false
