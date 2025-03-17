1b4ac1e3ef4fc7d1c35dd00429eada61708ddf9e
SananR <raosanan@gmail.com>
2025-03-16 21:10:04 -0400

    Production fix attempt

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true
+
+    # Override GlobalID's unique index check for this model.
+    def self.ensure_unique_index!
+        true
+    end
 end
--- config/initializers/global_id_patch.rb
+++
@@ -1,5 +0,0 @@
-GlobalID::Identification.module_eval do
-    def ensure_unique_index!
-      true
-    end
-end
