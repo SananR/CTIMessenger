66133cd91ab4100c627528cecc40441540f0aa56
SananR <raosanan@gmail.com>
2025-03-16 20:18:04 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
+  config.serve_static_assets = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
