bf120daddbf4c8dddf4e3667eab4bde299c381a5
SananR <raosanan@gmail.com>
2025-03-16 20:13:08 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
-  
-  config.assets.compile = false
+
+  config.assets.compile = true
 
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
