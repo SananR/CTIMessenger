fc9dd26490ae5d6730e04703f53066d68f8e2a82
SananR <raosanan@gmail.com>
2025-03-16 21:28:39 -0400

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:body)
+    params.require(:message).permit(:body, :id)
   end
 
   def message_render(message)
